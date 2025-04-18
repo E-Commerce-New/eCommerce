758f4ddc80f8debf1b86e50ecdeb5a9b95eccbb0
CuriousShivam <shivam.sah2023t@gmail.com>
2025-04-19 01:20:07 +0530

    Login Api Created

--- server/index.js
+++ server/index.js
@@ -1,11 +1,59 @@
-const express = require('express')
-const app = express()
-const port = 3000
+require("dotenv").config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3002;
+const mongoose = require('mongoose');
+const cors = require('cors');
 
+// Import the User model
+const User = require('./models/user');
+
+// CORS setup
+const corsOptions = {
+    origin: "*",
+    credentials: true,
+    optionSuccessStatus: 200,
+};
+app.use(express.json());
+app.use(cors(corsOptions));
+
+// Connect to MongoDB Atlas
+mongoose
+    .connect(process.env.MONGO_URI, {})
+    .then(() => console.log("MongoDB Connected"))
+    .catch((err) => console.error("MongoDB Connection Error:", err));
+
+// Root route
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+    res.send('Hello World!');
+});
 
+// Login or Registration route
+app.post('/api/user/login', async (req, res) => {
+    // console.log("Request body:", req.body);
+    const { email, password } = req.body;
+
+    try {
+        // Create a new user
+        let user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: "User not found" });
+          }
+        if(user.password === password){
+            res.status(200).json({ message: "User Logged In successfully...", user });
+
+        }else{
+            res.status(404).json({message:"User Not Found..."})
+        }
+    
+
+    } catch (error) {
+        console.error("Error saving user:", error);
+        res.status(500).json({ message: "Server error" });
+    }
+});
+
+// Start server
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Server running on port ${port}`);
+});
---
+++ server/models/user.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    // required: true,
+    trim: true,
+  },
+
+  email: {
+    type: String,
+    // required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+  },
+
+  phone: {
+    type: String,
+    default: null,
+  },
+
+  address: {
+    street: String,
+    city: String,
+    state: String,
+    postalCode: String,
+    country: String,
+  },
+
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+
+  cart: [
+    {
+      productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+      },
+      quantity: {
+        type: Number,
+        default: 1,
+      }
+    }
+  ],
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  }
+});
+
+module.exports = mongoose.model('User', userSchema);
