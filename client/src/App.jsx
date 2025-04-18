7a5f8c7156c796736270426b8cef2ffedbfa0e4d
CuriousShivam <shivam.sah2023t@gmail.com>
2025-04-18 21:52:28 +0530

    By shivam

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,9 @@
 export default function App() {
     return (
-        <h1 className="text-3xl font-bold underline">
+      <>  <h1 className="text-3xl font-bold underline">
             Something Big is coming soon.
         </h1>
+<h3>This is done by shivam</h3>
+	    </>
     )
 }
