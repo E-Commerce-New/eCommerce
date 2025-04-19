37c3593a48cdd5865d5a1fbc3c2b01dbd789bcc3
CuriousShivam <shivam.sah2023t@gmail.com>
2025-04-19 16:54:28 +0530

    Process Segrigation

--- server/index.js
+++ server/index.js
@@ -4,16 +4,11 @@ const app = express();
 const port = process.env.PORT || 3002;
 const mongoose = require('mongoose');
 const cors = require('cors');
+const userRouter = require('./routes/user.routes')
 
 // Import the User model
 const User = require('./models/user');
 
-// CORS setup
-// const corsOptions = {
-//     origin: "http://localhost:5173/",
-//     // credentials: true,
-//     // optionSuccessStatus: 200,
-// };
 app.use(express.json());
 app.use(cors());
 
@@ -23,35 +18,8 @@ mongoose
     .then(() => console.log("MongoDB Connected"))
     .catch((err) => console.error("MongoDB Connection Error:", err));
 
-// Root route
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
-// Login or Registration route
-app.post('/api/user/login', async (req, res) => {
-    console.log("Request body:", req.body);
-    const { username, password } = req.body;
-
-    try {
-        // Create a new user
-        let user = await User.findOne({ email : username });
-        if (!user) {
-            return res.status(400).json({ message: "User not found" });
-          }
-        if(user.password === password){
-            res.status(200).json({ message: "User Logged In successfully...", user });
-
-        }else{
-            res.status(404).json({message:"User Not Found..."})
-        }
-    
-
-    } catch (error) {
-        console.error("Error saving user:", error);
-        res.status(500).json({ message: "Server error" });
-    }
-});
+// Api routes
+app.use('/api/user', userRouter)
 
 // Start server
 app.listen(port, () => {
