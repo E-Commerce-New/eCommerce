2b753a6f73ce61e77c471fc71587cd8ae00c77bc
CuriousShivam <shivam.sah2023t@gmail.com>
2025-04-26 22:35:39 +0530

    Connected Create Order SR API

--- client/src/pages/customer/Cart.jsx
+++ client/src/pages/customer/Cart.jsx
@@ -14,6 +14,7 @@ const Cart = () => {
     const navigate = useNavigate();
     const [cartItems, setCartItems] = useState([]);
     const shippingAddress = user.addresses?.[0]
+    const [userCart, setUserCart] = useState([]);
 
     useEffect(() => {
         if (!user) {
@@ -33,6 +34,8 @@ const Cart = () => {
 
                 if (userRes.status === 200) {
                     const cart = userRes.data.data.cart;
+                    setUserCart(cart);
+                    // console.log('Cart: ',cart)
 
                     const productPromises = cart.map((item) => axios.post("http://localhost:3000/api/product/getProductById", {
                         id: item.productId,
@@ -42,7 +45,7 @@ const Cart = () => {
                     const products = productResponses.map((res, index) => ({
                         ...res.data.data, quantity: cart[index].quantity,
                     }));
-
+    // console.log('products ',products)
                     setCartItems(products);
                 }
             } catch (err) {
@@ -80,7 +83,6 @@ const Cart = () => {
             swal.close();
         }
     };
-
 
     const deleteCartItem = async (productId) => {
         Swal.fire({
@@ -125,72 +127,76 @@ const Cart = () => {
     };
 
     const handlePayment = async () => {
-        const res = await axios.post('http://localhost:3000/api/payment/create-order', {amount: totalPrice});
-        console.log("1st Api of Create Order" , res)
+        // const res = await axios.post('http://localhost:3000/api/payment/create-order', {amount: totalPrice});
+        // console.log("1st Api of Create Order" , res)
         // console.log(res.data.order.amount / 100)
 
-        const options = {
-            key: 'rzp_test_YkO4VIe1rAjpOw',
-            amount: res.data.order.amount,
-            currency: 'INR',
-            name: 'Ecommerce',
-            description: 'Store For You!',
-            order_id: res.data.order.id,
-            handler: async function (response) {
-                console.log(response);
-                const verifyRes = await axios.post('http://localhost:3000/api/payment/verify', {
-                    razorpay_order_id: response.razorpay_order_id,
-                    razorpay_payment_id: response.razorpay_payment_id,
-                    razorpay_signature: response.razorpay_signature,
-                    userId: user._id,
-                    amount: res.data.order.amount / 100,
-                });
-                console.log( "2nd Verify Api" , verifyRes);
+        // const options = {
+        //     key: 'rzp_test_YkO4VIe1rAjpOw',
+        //     amount: res.data.order.amount,
+        //     currency: 'INR',
+        //     name: 'Ecommerce',
+        //     description: 'Store For You!',
+        //     order_id: res.data.order.id,
+        //     handler: async function (response) {
+        //         console.log(response);
+        //         const verifyRes = await axios.post('http://localhost:3000/api/payment/verify', {
+        //             razorpay_order_id: response.razorpay_order_id,
+        //             razorpay_payment_id: response.razorpay_payment_id,
+        //             razorpay_signature: response.razorpay_signature,
+        //             userId: user._id,
+        //             amount: res.data.order.amount / 100,
+        //         });
+        //         console.log( "2nd Verify Api" , verifyRes);
+        //
+        //         if (verifyRes.data.success) {
+        //             swal.fire({
+        //                 title: 'Processing Your Order',
+        //                 allowOutsideClick: false,
+        //                 allowEscapeKey: false,
+        //                 didOpen: () => {
+        //                     Swal.showLoading();
+        //                 }
+        //             });
+        //             const placeRes = await axios.post('http://localhost:3000/api/order/place', {
+        //                 cartItems, shippingAddress, paymentInfo: response, totalPrice, userId: user._id,
+        //             });
+        //             console.log("3rd Save data Api" , placeRes);
+        //
+        //             if (placeRes.data.success) {
+        //                 swal.close()
+        //                 swal.fire("Success!", "Your order has been placed", "success").then((result) => {
+        //                     if (result.isConfirmed) {
+        //                         location.reload();
+        //                     }
+        //                 })
+        //             } else {
+        //                 swal.close()
+        //                 swal.fire({
+        //                     title: 'Error!',
+        //                     icon: 'error',
+        //                     text: placeRes.data.message,
+        //                 })
+        //             }
+        //         } else {
+        //             swal.fire("Failed", "Payment verification failed", "error");
+        //         }
+        //
+        //     },
+        //     prefill: {
+        //         name: user?.firstname + ' ' + user?.lastname, email: user?.email, contact: user.phone,
+        //     },
+        //     theme: {
+        //         color: '#3399cc',
+        //     },
+        // };
+        //
+        // const rzp = new window.Razorpay(options);
+        // rzp.open();
 
-                if (verifyRes.data.success) {
-                    swal.fire({
-                        title: 'Processing Your Order',
-                        allowOutsideClick: false,
-                        allowEscapeKey: false,
-                        didOpen: () => {
-                            Swal.showLoading();
-                        }
-                    });
-                    const placeRes = await axios.post('http://localhost:3000/api/order/place', {
-                        cartItems, shippingAddress, paymentInfo: response, totalPrice, userId: user._id,
-                    });
-                    console.log("3rd Save data Api" , placeRes);
-
-                    if (placeRes.data.success) {
-                        swal.close()
-                        swal.fire("Success!", "Your order has been placed", "success").then((result) => {
-                            if (result.isConfirmed) {
-                                location.reload();
-                            }
-                        })
-                    } else {
-                        swal.close()
-                        swal.fire({
-                            title: 'Error!',
-                            icon: 'error',
-                            text: placeRes.data.message,
-                        })
-                    }
-                } else {
-                    swal.fire("Failed", "Payment verification failed", "error");
-                }
-
-            },
-            prefill: {
-                name: user?.firstname + ' ' + user?.lastname, email: user?.email, contact: user.phone,
-            },
-            theme: {
-                color: '#3399cc',
-            },
-        };
-
-        const rzp = new window.Razorpay(options);
-        rzp.open();
+        await axios.post('http://localhost:3000/api/order/place', {
+                            cartItems, shippingAddress, totalPrice, userId: user._id,userCart
+                        });
     };
 
     const handleProduct = (id) => {
--- server/controllers/order.controller.js
+++ server/controllers/order.controller.js
@@ -3,35 +3,172 @@ const Order = require("../models/order");
 const User = require("../models/user");
 const { sendOrderConfirmationEmail } = require("../utils/sendMail");
 
+const axios = require("axios");
+
+const getValidDateAndTime=()=>{
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, '0'); // months are 0-indexed
+    const day = String(now.getDate()).padStart(2, '0');
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    return (`${year}-${month}-${day} ${hours}:${minutes}`);
+}
+
 const placeOrder = async (req, res) => {
-    const { cartItems, shippingAddress, paymentInfo, totalPrice, userId } = req.body;
-    // console.log("total amount", req.body);
+    const { cartItems, shippingAddress, paymentInfo, totalPrice, userId, userCart} = req.body;
+    console.log("Req body", req.body);
 
     try {
-        const order = await Order.create({
-            userId: userId,
-            status: "Processing",
-            items: cartItems,
-            shippingAddress,
-            paymentMethod: paymentInfo?.method || "Unknown",
-            paymentStatus: "Paid",
-            transactionId: paymentInfo?.transactionId || "Pending",
-            total: totalPrice,
-            billingAddress: {
-                addressLine1 : "Sonia Vihar",
-                addressLine2 : "1st Pusta",
-                city : "Delhi",
-                state : "Delhi",
-                postalCode : '110094',
-                country: "India"
-            },
-        });
-        // console.log("Order Saved", order)
+        // console.log(req.body.cartItems);
+    const user = await User.findOne({_id: userId});
+
+        for(let i = 0; i < cartItems.length; i++){
+        //     const dataAndTime = getValidDateAndTime();
+        //     const data = await Order.create({
+        //         userId: userId,
+        //         status: "Processing",
+        //         items: cartItems[i],
+        //         shippingAddress,
+        //         paymentMethod: paymentInfo?.method || "Unknown",
+        //         paymentStatus: "Paid",
+        //         transactionId: paymentInfo?.transactionId || "Pending",
+        //         total: totalPrice,
+        //         billingAddress: {
+        //             addressLine1 : "Sonia Vihar",
+        //             addressLine2 : "1st Pusta",
+        //             city : "Delhi",
+        //             state : "Delhi",
+        //             postalCode : '110094',
+        //             country: "India",
+        //             userCart
+        //         },
+        //     })
+
+            // console.log("Data " , data);
+            const data = {
+                order_id:userCart[0]._id,
+                order_date: getValidDateAndTime(),
+                billing_customer_name:user.firstname ,
+                billing_last_name: user.lastname,
+                billing_address: shippingAddress.addressLine1,
+                billing_city: shippingAddress.city,
+                billing_pincode: Number(shippingAddress.postalCode),
+                billing_state:shippingAddress.state,
+                billing_country:"India",
+                billing_email: user.email,
+                billing_phone: Number(user.phone),
+                shipping_is_billing:true,
+                order_items:[
+                    {
+                    name: cartItems[i].name,
+                    units: cartItems[i].quantity,
+                    selling_price: cartItems[i].price,
+                    sku: cartItems[i].name
+                    }
+                    ],
+                payment_method: "Prepaid",
+                shipping_charges: 0,
+                giftwrap_charges: 0,
+                transaction_charges: 0,
+                total_discount: 0,
+                sub_total: cartItems[i].price,
+                length: cartItems[i].length,
+                breadth: cartItems[i].breadth,
+                height: cartItems[i].height,
+                weight: cartItems[i].weight
+            }
+            // console.log("Data", data);
+
+            const res = await axios.post("https://apiv2.shiprocket.in/v1/external/orders/create/adhoc", data, {
+                headers:{
+                    'content-type': 'application/json',
+                    Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjY0MDU5ODEsInNvdXJjZSI6InNyLWF1dGgtaW50IiwiZXhwIjoxNzQ2NDI3OTYzLCJqdGkiOiJMSmw3eDJNbUJNNTV4VWpkIiwiaWF0IjoxNzQ1NTYzOTYzLCJpc3MiOiJodHRwczovL3NyLWF1dGguc2hpcHJvY2tldC5pbi9hdXRob3JpemUvdXNlciIsIm5iZiI6MTc0NTU2Mzk2MywiY2lkIjo2MTg2NzAwLCJ0YyI6MzYwLCJ2ZXJib3NlIjpmYWxzZSwidmVuZG9yX2lkIjowLCJ2ZW5kb3JfY29kZSI6IiJ9.uGlIvFc-hFkb3Ikm_7jHXYvbrg2dwzZpbVZTHsYGies'
+                }
+            } )
+
+            console.log("Response", res);
+
+            const order = await Order.create({
+                userId: userId,
+                order_id: res.data.order_id,
+                shipment_id: res.data.shipment_id,
+                status: res.data.status,
+                status_code: res.data.status_code,
+                onboarding_completed_Now: res.data.onboarding_completed_now,
+                awb_code: res.data.awb_code,
+                courier_company_id: res.data.courier_company_id,
+                new_channel: res.data.new_channel,
+                packaging_box_error:res.data.packaging_box_error,
+                order_items:[
+                    {
+                        name: cartItems[i].name,
+                        units: cartItems[i].quantity,
+                        selling_price: cartItems[i].price,
+                        sku: cartItems[i].name
+                    }
+                ],
+                shippingAddress,
+                paymentMethod: paymentInfo?.method || "Prepaid",
+                paymentStatus: "Paid",
+                transactionId: paymentInfo?.transactionId || "Pending",
+                total: cartItems[i].price,
+                billingAddress: {
+                    addressLine1 : "Sonia Vihar",
+                    addressLine2 : "1st Pusta",
+                    city : "Delhi",
+                    state : "Delhi",
+                    postalCode : '110094',
+                    country: "India"
+                },
+            });
+            console.log(order)
+        }
+
 
         await User.findByIdAndUpdate(userId, { cart: [] });
-        await sendOrderConfirmationEmail(process.env.Your_Email, order.toObject());
+        // await sendOrderConfirmationEmail(process.env.Your_Email, order.toObject());
 
-        res.status(200).json({ success: true, order });
+
+        // { order_id : number min-6 numbers,
+        //     order_date:  string 2019-07-24 11:11,
+        //     billing_customer_name: string,
+        //     billling_address: string,
+        //     billing_city: string,
+        //     billing_pincode: integer,
+        //     billing_state:string,
+        //     billing_country:string,
+        //     billing_email:string,
+        //     billing_phone:integer 8368509006,
+        //     shipping_is_billing:boolean,
+        //     shipping_customer_name:(CONDITIONAL string( Required in case billing is not same as shipping.) and a few more if billing is not shipping),
+        //     order_items:array(List of items and their relevant fields in the form of Array.),
+        //     name:string(product name),
+        //     sku : string(product sku),
+        //     selling_price: integer(inclusive of GST),
+        //     payent_method: stirng(cod or prepaid),
+        //     sub_total: integer,
+        //         length: float(in cm),
+        //     bredth: float(in cm),
+        //     height: flaot(in cm),
+        //     weight: float( in kg)
+        // }
+
+        //
+    //     "order_id": 818383995,
+    //         "channel_order_id": "224-446",
+    //         "shipment_id": 814768418,
+    //         "status": "NEW",
+    //         "status_code": 1,
+    //         "onboarding_completed_now": 0,
+    //         "awb_code": "",
+    //         "courier_company_id": "",
+    //         "courier_name": "",
+    //         "new_channel": false,
+    //         "packaging_box_error": ""
+    // }
+
+        res.status(200).json({ success: true});
     } catch (err) {
         console.error("Error placing order:", err);
         res.status(500).json({ success: false, message: err.message });
