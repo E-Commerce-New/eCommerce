0333fc19a5d8293b49142c2e1fa5ce816d2845bb
CuriousShivam <shivam.sah2023t@gmail.com>
2025-05-06 13:12:07 +0530

    Committing to take a pull

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors(
     {
-    origin: 'http://localhost:5173', // your frontend origin
+    origin: process.env.ORIGIN, // your frontend origin
     credentials: true
 }
 ));
